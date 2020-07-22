8563ef84a84fc80183453dc2ad7163c8be94f28c
Elliott Loveridge <el627@bath.ac.uk>
2020-07-22 18:14:11 +0100

    Indent update

--- efficient-3d-cnn/utils/video_jpg_ucf101_hmdb51.py
+++ efficient-3d-cnn/utils/video_jpg_ucf101_hmdb51.py
@@ -24,24 +24,24 @@ def class_process(dir_path, dst_dir_path, class_name):
         dst_directory_path = os.path.join(dst_class_path, name)
         video_file_path = os.path.join(class_path, file_name)
 
-    try:
-        if os.path.exists(dst_directory_path):
-            if not os.path.exists(os.path.join(dst_directory_path, 'image_00001.jpg')):
-                subprocess.call('rm -r \"{}\"'.format(dst_directory_path), shell=True)
-                print('remove {}'.format(dst_directory_path))
-                os.mkdir(dst_directory_path)
+        try:
+            if os.path.exists(dst_directory_path):
+                if not os.path.exists(os.path.join(dst_directory_path, 'image_00001.jpg')):
+                    subprocess.call('rm -r \"{}\"'.format(dst_directory_path), shell=True)
+                    print('remove {}'.format(dst_directory_path))
+                    os.mkdir(dst_directory_path)
+                else:
+                    continue
             else:
-                continue
-        else:
-            os.mkdir(dst_directory_path)
-    except:
-        print(dst_directory_path)
-        continue
+                os.mkdir(dst_directory_path)
+        except:
+            print(dst_directory_path)
+            continue
 
-    cmd = 'ffmpeg -i \"{}\" -vf scale=-1:240 \"{}/image_%05d.jpg\"'.format(video_file_path, dst_directory_path)
-    print(cmd)
-    subprocess.call(cmd, shell=True)
-    print('\n')
+        cmd = 'ffmpeg -i \"{}\" -vf scale=-1:240 \"{}/image_%05d.jpg\"'.format(video_file_path, dst_directory_path)
+        print(cmd)
+        subprocess.call(cmd, shell=True)
+        print('\n')
 
 
 if __name__=="__main__":
