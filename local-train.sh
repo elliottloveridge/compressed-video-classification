cc35719dc2e4d5bf1c1bb7c91a75ce974e317f42
Elliott Loveridge <el627@bath.ac.uk>
2020-07-27 14:37:57 +0100

    Updates

--- local-train.sh
+++ local-train.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python3 /app/efficient-3d-cnn/main.py --video_path ~/video-classification/toy/train --annotation_path ~/Documents/GitHub/research-project/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --width_mult 1.0
+python3 /app/efficient-3d-cnn/main.py --root_path ~/ --video_path ~/video-classification/toy/train --annotation_path ~/Documents/GitHub/research-project/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --width_mult 1.0
