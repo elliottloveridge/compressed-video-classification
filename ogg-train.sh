6d205dcf26d0a5a93848f71e6f23437db9cf5932
Elliott Loveridge <el627@bath.ac.uk>
2020-07-30 15:48:42 +0100

    ogg-train, added second mount of data directory

--- ogg-train.sh
+++ ogg-train.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /app/efficient-3d-cnn/main.py --root_path /mnt/slow0/ucf101/data/ --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --batch_size 2  --model mobilenetv2 --width_mult 1.0 --n_val_samples 1
+python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --batch_size 2  --model mobilenetv2 --width_mult 1.0 --n_val_samples 1
