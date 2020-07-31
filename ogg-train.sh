fdb2c981c367b2d908a106bfb26f7cedec7aeaca
Elliott Loveridge <el627@bath.ac.uk>
2020-07-31 14:27:38 +0100

    Created a separate folder for quantization

--- ogg-train.sh
+++ ogg-train.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --batch_size 32  --model mobilenetv2 --width_mult 1.0 --n_val_samples 1 --checkpoint 5
+python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --batch_size 32  --model mobilenetv2 --width_mult 1.0 --n_val_samples 1 --checkpoint 5 --n_epochs 20
