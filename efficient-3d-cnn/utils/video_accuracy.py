b4e22f0f3309257610283cbb47a89ae7ee5f1bdb
Elliott Loveridge <el627@bath.ac.uk>
2020-08-07 12:18:16 +0100

    Removed opts from eval script

--- efficient-3d-cnn/utils/video_accuracy.py
+++ efficient-3d-cnn/utils/video_accuracy.py
@@ -1,24 +1,28 @@
 from eval_ucf101 import UCFclassification
 from eval_kinetics import KINETICSclassification
 
-from opts import parse_opts
+# from opts import parse_opts
 
-opt = parse_opts()
-if opt.root_path != '':
-    opt.annotation_path = os.path.join(opt.root_path, opt.annotation_path)
-    opt.result_path = os.path.join(opt.root_path, opt.result_path)
+# opt = parse_opts()
+# if opt.root_path != '':
+#     opt.annotation_path = os.path.join(opt.root_path, opt.annotation_path)
+#     opt.result_path = os.path.join(opt.root_path, opt.result_path)
+#
+# print(opt)
+# # FIXME: add an opt for top_k = 1 or 5 so you can set it!
 
-print(opt)
-# FIXME: add an opt for top_k = 1 or 5 so you can set it!
+dataset = 'ucf101'
+result_path = '/data/results/val.json'
+annotation_path = '/app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json'
 
-if opt.dataset == 'ucf101':
+if dataset == 'ucf101':
 
     # check if you need str() for paths
-    ucf_classification = UCFclassification(opt.annotation__path, opt.result_path, subset='validation', top_k=1)
+    ucf_classification = UCFclassification(annotation__path, result_path, subset='validation', top_k=1)
     ucf_classification.evaluate()
     print(ucf_classification.hit_at_k)
 
-elif opt.dataset == 'kinetics':
+elif dataset == 'kinetics':
     kinetics_classification = KINETICSclassification('../annotation_Kinetics/kinetics.json',
                                            '../results/val.json',
                                            subset='validation',
