3dbf1a565eafac7ee3d4ff3b9c0f00ac49293e24
Elliott Loveridge <el627@bath.ac.uk>
2020-08-07 12:07:06 +0100

    Added video accuracy for ucf101 script

--- efficient-3d-cnn/utils/video_accuracy.py
+++ efficient-3d-cnn/utils/video_accuracy.py
@@ -1,19 +1,26 @@
 from eval_ucf101 import UCFclassification
 from eval_kinetics import KINETICSclassification
 
+opt = parse_opts()
+if opt.root_path != '':
+    opt.annotation_path = os.path.join(opt.root_path, opt.annotation_path)
+    opt.result_path = os.path.join(opt.root_path, opt.result_path)
 
+print(opt)
+# FIXME: add an opt for top_k = 1 or 5 so you can set it!
 
-# ucf_classification = UCFclassification('../annotation_UCF101/ucf101_01.json',
-#                                        '../results/val.json',
-#                                        subset='validation', top_k=1)
-# ucf_classification.evaluate()
-# print(ucf_classification.hit_at_k)
+if opt.dataset == 'ucf101':
 
+    # check if you need str() for paths
+    ucf_classification = UCFclassification(opt.annotation__path, opt.result_path, subset='validation', top_k=1)
+    ucf_classification.evaluate()
+    print(ucf_classification.hit_at_k)
 
-kinetics_classification = KINETICSclassification('../annotation_Kinetics/kinetics.json',
-                                       '../results/val.json',
-                                       subset='validation',
-                                       top_k=1,
-                                       check_status=False)
-kinetics_classification.evaluate()
-print(kinetics_classification.hit_at_k)
+elif opt.dataset == 'kinetics':
+    kinetics_classification = KINETICSclassification('../annotation_Kinetics/kinetics.json',
+                                           '../results/val.json',
+                                           subset='validation',
+                                           top_k=1,
+                                           check_status=False)
+    kinetics_classification.evaluate()
+    print(kinetics_classification.hit_at_k)
---
+++ ogg-eval.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --dataset ucf101 --result_path results
