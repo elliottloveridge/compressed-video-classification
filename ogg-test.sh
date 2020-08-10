ce54e839a5ed95dd892b7f89612bf3c84295c4e9
Elliott Loveridge <el627@bath.ac.uk>
2020-08-10 10:08:01 +0100

    Updated test path for checkpoint train to get val.json

--- ogg-test.sh
+++ ogg-test.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results/qat/0608 --dataset ucf101 --n_classes 101 --n_epochs 20 --model mobilenetv2 --test --no_train --no_val --resume_path results/qat/0608/ucf101_mobilenetv2_20epochs_16_qat-0608_best.pth
+python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --test --no_train --no_val --resume_path results/ucf101_mobilenetv2_20epochs_32batch-size_train-0608_checkpoint.pth
