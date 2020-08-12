d080d62c7306a45d43b2ccd371c40575ac4c1c0d
Elliott Loveridge <el627@bath.ac.uk>
2020-08-12 11:18:39 +0100

    test script typo

--- ogg-test.sh
+++ ogg-test.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /app/quantization-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --test --no_train --no_val --resume_path results/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
+python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --test --no_train --no_val --resume_path results/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
