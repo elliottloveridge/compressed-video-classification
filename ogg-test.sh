0fcb359c03dad8ac3a2b4819b4a7a782f07aca6d
Elliott Loveridge <el627@bath.ac.uk>
2020-08-14 12:09:51 +0100

    Update test script

--- ogg-test.sh
+++ ogg-test.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --test --no_train --no_val --resume_path results/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
+python /app/efficient-3d-cnn/main.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/efficient-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results/benchmark/1108 --dataset ucf101 --n_classes 101 --model mobilenetv2 --test --no_train --no_val --resume_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
