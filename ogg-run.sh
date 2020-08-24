60a5b98b2ca5fd7f576008b289b2cc78d739c1c8
Elliott Loveridge <el627@bath.ac.uk>
2020-08-24 09:16:00 +0100

    senstivity analysis on new model

--- ogg-run.sh
+++ ogg-run.sh
@@ -22,10 +22,9 @@
 
 ## other
 
-# DEBUG: not tested
-# distiller model summary
-python /app/quantization-3d-cnn/model-summary.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/quantization-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --width_mult 1.0 --resume_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
+# # distiller model summary - uses resume_path
+# python /app/quantization-3d-cnn/model-summary.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/quantization-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --model mobilenetv2 --width_mult 1.0 --resume_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
 
-# # DEBUG: this saves a val.json into results that isn't needed
-# # distiller pruning sensitivity analysis
-# python /app/quantization-3d-cnn/model-sensitivity.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/quantization-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --batch_size 32  --model mobilenetv2 --width_mult 1.0 --n_val_samples 1 --n_epochs 1 --resume_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
+# DEBUG: this saves a val.json into results that isn't needed
+# distiller pruning sensitivity analysis
+python /app/quantization-3d-cnn/model-sensitivity.py --root_path /data --video_path ucf101_videos/jpg/ --annotation_path /app/quantization-3d-cnn/annotation_UCF101/ucf101_01.json --result_path results --dataset ucf101 --n_classes 101 --batch_size 32  --model mobilenetv2 --width_mult 1.0 --n_val_samples 1 --n_epochs 1
