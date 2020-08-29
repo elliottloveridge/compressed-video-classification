c6ff55dd9adcc1d0347ea50ba3b7561e2b3575a6
Elliott Loveridge <el627@bath.ac.uk>
2020-08-29 18:57:14 +0100

    added .cuda() to loop

--- compressed-3d-cnn/calculate_FLOP.py
+++ compressed-3d-cnn/calculate_FLOP.py
@@ -32,7 +32,8 @@ def model_info(model, opt):
     # # NOTE: if these two lines are needed, in what order should they go?
     # model = model.cuda()
     # model = nn.DataParallel(model)
-    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    # added .cuda() to the below
+    pytorch_total_params = sum(p.numel().cuda() for p in model.parameters() if p.requires_grad)
     flops, prms = profile(model, input_size=(1, 3, 16, 112, 112))
 
     return pytorch_total_params, flops
