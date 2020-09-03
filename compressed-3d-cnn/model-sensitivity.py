7229f0daf567de779a049134ebf3b0d71b53b875
Elliott Loveridge <el627@bath.ac.uk>
2020-09-03 12:47:09 +0100

    model sense error print

--- compressed-3d-cnn/model-sensitivity.py
+++ compressed-3d-cnn/model-sensitivity.py
@@ -60,6 +60,7 @@ def perform_sensitivity_analysis(model, net_params, sparsities, test_func, group
 
     for param_name in net_params:
         if model.state_dict()[param_name].dim() not in [2,4]:
+            print('here')
             continue
 
         # Make a copy of the model, because when we apply the zeros mask (i.e.
