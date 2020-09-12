f3e892b90c54991ef201ebe1cb39eb527cad9e83
Elliott Loveridge <el627@bath.ac.uk>
2020-09-12 10:22:13 +0100

    element wise pruning test

--- ogg-run.sh
+++ ogg-run.sh
@@ -71,25 +71,25 @@
 #   --n_epochs 20 \
 #   --test
 
-# ucf101-resnet101 (inc. testing) - 20 epochs, 0.01 learning rate, 5 checkpoint, kinetics pre-train
-python /app/compressed-3d-cnn/main.py --root_path /data \
-  --video_path ucf101_videos/jpg/ \
-  --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-  --result_path results \
-  --pretrain_path /data/results/pretrain/kinetics_resnet_101_RGB_16_best.pth \
-  --dataset ucf101 \
-  --n_classes 600 \
-  --n_finetune_classes 101 \
-  --ft_portion complete \
-  --model resnet \
-  --model_depth 101 \
-  --learning_rate 0.01 \
-  --sample_duration 16 \
-  --batch_size 32 \
-  --checkpoint 5 \
-  --n_val_samples 1 \
-  --n_epochs 20 \
-  --test
+# # ucf101-resnet101 (inc. testing) - 20 epochs, 0.01 learning rate, 5 checkpoint, kinetics pre-train
+# python /app/compressed-3d-cnn/main.py --root_path /data \
+#   --video_path ucf101_videos/jpg/ \
+#   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+#   --result_path results \
+#   --pretrain_path /data/results/pretrain/kinetics_resnet_101_RGB_16_best.pth \
+#   --dataset ucf101 \
+#   --n_classes 600 \
+#   --n_finetune_classes 101 \
+#   --ft_portion complete \
+#   --model resnet \
+#   --model_depth 101 \
+#   --learning_rate 0.01 \
+#   --sample_duration 16 \
+#   --batch_size 32 \
+#   --checkpoint 5 \
+#   --n_val_samples 1 \
+#   --n_epochs 20 \
+#   --test
 
 
 ## compression
@@ -103,24 +103,24 @@ python /app/compressed-3d-cnn/main.py --root_path /data \
 
 ## element-wise pruning
 
-# # ucf101-mobilenetv2-ep (inc. testing) - 1 epoch, 16 batch_size, 0.1 learning rate, no checkpoint
-# python /app/compressed-3d-cnn/main.py --root_path /data \
-#   --video_path ucf101_videos/jpg/ \
-#   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-#   --result_path results \
-#   --resume_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
-#   --dataset ucf101 \
-#   --n_classes 101 \
-#   --batch_size 16  \
-#   --model mobilenetv2 \
-#   --width_mult 1.0 \
-#   --learning_rate 0.1 \
-#   --n_val_samples 1 \
-#   --n_epochs 1 \
-#   --test \
-#   --compress \
-#   --compression_type ep \
-#   --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2.yaml
+# ucf101-mobilenetv2-ep (inc. testing) - 1 epoch, 16 batch_size, 0.1 learning rate, no checkpoint
+python /app/compressed-3d-cnn/main.py --root_path /data \
+  --video_path ucf101_videos/jpg/ \
+  --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+  --result_path results \
+  --resume_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
+  --dataset ucf101 \
+  --n_classes 101 \
+  --batch_size 16  \
+  --model mobilenetv2 \
+  --width_mult 1.0 \
+  --learning_rate 0.1 \
+  --n_val_samples 1 \
+  --n_epochs 1 \
+  --test \
+  --compress \
+  --compression_type ep \
+  --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2.yaml
 
 
 ## knowledge distillation
