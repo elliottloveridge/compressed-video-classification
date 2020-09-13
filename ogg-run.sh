da12046773a501e88d412a71976d16b84b6622f5
Elliott Loveridge <el627@bath.ac.uk>
2020-09-13 10:53:13 +0100

    sparsity level parameter pruning test

--- ogg-run.sh
+++ ogg-run.sh
@@ -119,7 +119,7 @@ python /app/compressed-3d-cnn/main.py --root_path /data \
   --n_epochs 1 \
   --compress \
   --compression_type ep \
-  --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2.yaml
+  --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2-2.yaml
 
 
 ## knowledge distillation
