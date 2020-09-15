5000e699d2aeadc00a14e7e47ab2fbb4b7eb615d
Elliott Loveridge <el627@bath.ac.uk>
2020-09-15 09:22:45 +0100

    sensitivity pruning from scratch

--- ogg-run.sh
+++ ogg-run.sh
@@ -129,7 +129,7 @@ python /app/compressed-3d-cnn/main.py --root_path /data \
   --video_path ucf101_videos/jpg/ \
   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
   --result_path results \
-  --resume_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
+  # --resume_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
   --dataset ucf101 \
   --n_classes 101 \
   --batch_size 16  \
@@ -140,7 +140,7 @@ python /app/compressed-3d-cnn/main.py --root_path /data \
   --n_epochs 1 \
   --compress \
   --compression_type ep \
-  --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2-2.yaml
+  --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2.yaml
 
 
 ## knowledge distillation
