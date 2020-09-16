552f0bb20f7664fb904f6533c9d618d55c4760a2
Elliott Loveridge <el627@bath.ac.uk>
2020-09-16 14:23:59 +0100

    typo

--- compressed-3d-cnn/main-prune.py
+++ compressed-3d-cnn/main-prune.py
@@ -278,8 +278,7 @@ if __name__ == '__main__':
                               lr=optimizer.param_groups[0]['lr']))
 
                     spar = sum(distiller.utils.sparsity(p.data) for p in model.parameters() if p.requires_grad)
-
-                    print('epoch {0}, sum of weight sparsity: {1}'.format(epoch, spar)
+                    print('epoch {0}, sum of weight sparsity: {1}'.format(epoch, spar))
 
             train_logger.log({
                 'epoch': i,
