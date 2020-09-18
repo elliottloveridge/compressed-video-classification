058ae0a2b1c1d4a327a6da7261eee42eb1a6ddad
Elliott Loveridge <el627@bath.ac.uk>
2020-09-18 08:35:39 +0100

    add resnet shortcut to ResNet-18 fine-tune

--- ogg-run.sh
+++ ogg-run.sh
@@ -103,6 +103,7 @@ python /app/compressed-3d-cnn/main.py --root_path /data \
   --batch_size 32 \
   --checkpoint 5 \
   --downsample 1 \
+  --resnet_shortcut A \
   --n_val_samples 1 \
   --n_epochs 20 \
   --test
