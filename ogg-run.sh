709cd04ca77af97942b3f617695da19685f1962f
Elliott Loveridge <el627@bath.ac.uk>
2020-09-20 14:20:19 +0100

    pruning test 0.5 conv layer sparsity

--- ogg-run.sh
+++ ogg-run.sh
@@ -180,23 +180,23 @@
 
 ## element-wise pruning
 
-# # ucf101-mobilenetv2-ep (inc. testing) - 1 epoch, 16 batch_size, 0.1 learning rate, no checkpoint
-# python /app/compressed-3d-cnn/main.py --root_path /data \
-#   --video_path ucf101_videos/jpg/ \
-#   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-#   --result_path results \
-#   --dataset ucf101 \
-#   --n_classes 101 \
-#   --batch_size 16  \
-#   --model mobilenetv2 \
-#   --width_mult 1.0 \
-#   --learning_rate 0.1 \
-#   --n_val_samples 1 \
-#   --n_epochs 20 \
-#   --compress \
-#   --compression_type ep \
-#   --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2.yaml \
-#   --test
+# ucf101-mobilenetv2-ep (inc. testing) - 1 epoch, 16 batch_size, 0.1 learning rate, no checkpoint
+python /app/compressed-3d-cnn/main.py --root_path /data \
+  --video_path ucf101_videos/jpg/ \
+  --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+  --result_path results \
+  --dataset ucf101 \
+  --n_classes 101 \
+  --batch_size 16  \
+  --model mobilenetv2 \
+  --width_mult 1.0 \
+  --learning_rate 0.1 \
+  --n_val_samples 1 \
+  --n_epochs 5 \
+  --compress \
+  --compression_type ep \
+  --compression_file /app/compressed-3d-cnn/distiller/ep-mobilenetv2-test1.yaml \
+  --test
 
 
 ## knowledge distillation
@@ -281,20 +281,20 @@
 # --n_epochs 1
 
 
-# distiller new pruning file
-python /app/compressed-3d-cnn/new-prune.py --root_path /data \
-  --video_path ucf101_videos/jpg/ \
-  --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-  --result_path results \
-  --dataset ucf101 \
-  --n_classes 101 \
-  --batch_size 32  \
-  --model mobilenetv2 \
-  --width_mult 1.0 \
-  --learning_rate 0.1 \
-  --n_val_samples 1 \
-  --n_epochs 1 \
-  --pretrain_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
+# # distiller new pruning file
+# python /app/compressed-3d-cnn/new-prune.py --root_path /data \
+#   --video_path ucf101_videos/jpg/ \
+#   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+#   --result_path results \
+#   --dataset ucf101 \
+#   --n_classes 101 \
+#   --batch_size 32  \
+#   --model mobilenetv2 \
+#   --width_mult 1.0 \
+#   --learning_rate 0.1 \
+#   --n_val_samples 1 \
+#   --n_epochs 1 \
+#   --pretrain_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
 
 
 # # distller pruning test only
