3908b03a620c90f8b49fc29bbce8254d438796d7
Elliott Loveridge <el627@bath.ac.uk>
2020-09-24 09:31:01 +0100

    test run for hyper-parameter search kd

--- ogg-run.sh
+++ ogg-run.sh
@@ -115,23 +115,23 @@
 
 ## qat
 
-# ucf101-mobilenetv2-qat (inc. testing) - 20 epochs, 0.01 learning rate, no checkpoint
-python /app/compressed-3d-cnn/main.py --root_path /data \
---video_path ucf101_videos/jpg/ \
---annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
---result_path results \
---dataset ucf101 \
---n_classes 101 \
---batch_size 32  \
---model csn \
---model_depth 50 \
---learning_rate 0.1 \
---n_val_samples 1 \
---n_epochs 2 \
---test \
---compress \
---compression_type qat \
---compression_file /app/compressed-3d-cnn/distiller/linear-qat.yaml
+# # ucf101-mobilenetv2-qat (inc. testing) - 20 epochs, 0.01 learning rate, no checkpoint
+# python /app/compressed-3d-cnn/main.py --root_path /data \
+# --video_path ucf101_videos/jpg/ \
+# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+# --result_path results \
+# --dataset ucf101 \
+# --n_classes 101 \
+# --batch_size 32  \
+# --model csn \
+# --model_depth 50 \
+# --learning_rate 0.1 \
+# --n_val_samples 1 \
+# --n_epochs 2 \
+# --test \
+# --compress \
+# --compression_type qat \
+# --compression_file /app/compressed-3d-cnn/distiller/linear-qat.yaml
 
 # # ucf101-mobilenetv2-qat-fine-tuning (inc. testing) - 20 epochs, 0.01 learning_rate, 5 checkpoint
 # python /app/compressed-3d-cnn/main.py --root_path /data \
@@ -201,54 +201,30 @@ python /app/compressed-3d-cnn/main.py --root_path /data \
 
 ## knowledge distillation
 
-# # ucf101-resnet-101 to resnet-18 knowledge distillation training (inc. testing)
-# # NOTE: t_n_classes set as 101 as using fine-tuned teacher model
-# python /app/compressed-3d-cnn/distillation.py --root_path /data \
-# --video_path ucf101_videos/jpg/ \
-# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-# --result_path results \
-# --dataset ucf101 \
-# --n_classes 101 \
-# --batch_size 16 \
-# --model resnet \
-# --model_depth 18 \
-# --learning_rate 0.1 \
-# --n_val_samples 1 \
-# --n_epochs 5 \
-# --compress \
-# --compression_type kd \
-# --t_model resnet \
-# --t_model_depth 101 \
-# --t_path /data/results/benchmark/1209/ucf101_resnet_benchmark_20epochs_1209_best.pth \
-# --t_n_classes 101 \
-# --kd_distill_wt 0.7 \
-# --kd_student_wt 0.3 \
-# --kd_temp 5.0 \
-# --test
-
-# # ucf101-resnet-101 to MobileNetV2 knowledge distillation training (inc. testing)
-# # NOTE: t_n_classes set as 101 as using fine-tuned teacher model
-# python /app/compressed-3d-cnn/distillation.py --root_path /data \
-# --video_path ucf101_videos/jpg/ \
-# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-# --result_path results \
-# --dataset ucf101 \
-# --n_classes 101 \
-# --batch_size 16 \
-# --model mobilenetv2 \
-# --learning_rate 0.1 \
-# --n_val_samples 1 \
-# --n_epochs 5 \
-# --compress \
-# --compression_type kd \
-# --t_model resnet \
-# --t_model_depth 101 \
-# --t_path /data/results/benchmark/1209/ucf101_resnet_benchmark_20epochs_1209_best.pth \
-# --t_n_classes 101 \
-# --kd_distill_wt 0.7 \
-# --kd_student_wt 0.3 \
-# --kd_temp 5.0 \
-# --test
+# ucf101-resnet-101 to resnet-18 knowledge distillation training (inc. testing)
+# NOTE: t_n_classes set as 101 as using fine-tuned teacher model
+python /app/compressed-3d-cnn/distillation.py --root_path /data \
+--video_path ucf101_videos/jpg/ \
+--annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+--result_path results \
+--dataset ucf101 \
+--n_classes 101 \
+--batch_size 16 \
+--model resnet \
+--model_depth 18 \
+--learning_rate 0.1 \
+--n_val_samples 1 \
+--n_epochs 5 \
+--compress \
+--compression_type kd \
+--t_model resnet \
+--t_model_depth 101 \
+--t_path /data/results/benchmark/1209/ucf101_resnet_benchmark_20epochs_1209_best.pth \
+--t_n_classes 101 \
+--kd_distill_wt 0.7 \
+--kd_student_wt 0.3 \
+--kd_temp 5.0 \
+--test
 
 
 ## other
