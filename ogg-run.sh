bc5f1d3921aab9e2ba718f63b67b8f060651ca53
Elliott Loveridge <el627@bath.ac.uk>
2020-09-26 17:28:46 +0100

    eval for csn kd

--- ogg-run.sh
+++ ogg-run.sh
@@ -58,11 +58,11 @@
 
 ## evaluation
 
-# # ucf101 evaluation (after testing)
-# python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
-# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-# --dataset ucf101 \
-# --result_path results
+# ucf101 evaluation (after testing)
+python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
+--annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+--dataset ucf101 \
+--result_path results
 
 
 ## fine-tuning (pre-trained)
@@ -201,31 +201,31 @@
 
 ## knowledge distillation
 
-# ucf101-resnet-101 to resnet-18 knowledge distillation training (inc. testing)
-# NOTE: t_n_classes set as 101 as using fine-tuned teacher model
-python /app/compressed-3d-cnn/main.py --root_path /data \
---video_path ucf101_videos/jpg/ \
---annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
---result_path results \
---dataset ucf101 \
---n_classes 101 \
---batch_size 8 \
---model csn \
---model_depth 50 \
---learning_rate 0.01 \
---n_val_samples 1 \
---n_epochs 20 \
---compress \
---compression_type kd \
---t_model resnet \
---t_model_depth 101 \
---t_path /data/results/benchmark/1209/ucf101_resnet_benchmark_20epochs_1209_best.pth \
---t_n_classes 101 \
---kd_distill_wt 0.7 \
---kd_student_wt 0.3 \
---kd_temp 3.0 \
---checkpoint 5 \
---test
+# # ucf101-resnet-101 to resnet-18 knowledge distillation training (inc. testing)
+# # NOTE: t_n_classes set as 101 as using fine-tuned teacher model
+# python /app/compressed-3d-cnn/main.py --root_path /data \
+# --video_path ucf101_videos/jpg/ \
+# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+# --result_path results \
+# --dataset ucf101 \
+# --n_classes 101 \
+# --batch_size 8 \
+# --model csn \
+# --model_depth 50 \
+# --learning_rate 0.01 \
+# --n_val_samples 1 \
+# --n_epochs 20 \
+# --compress \
+# --compression_type kd \
+# --t_model resnet \
+# --t_model_depth 101 \
+# --t_path /data/results/benchmark/1209/ucf101_resnet_benchmark_20epochs_1209_best.pth \
+# --t_n_classes 101 \
+# --kd_distill_wt 0.7 \
+# --kd_student_wt 0.3 \
+# --kd_temp 3.0 \
+# --checkpoint 5 \
+# --test
 
 
 ## other
