c1db15800ba1224ad22e81ff2598b85eb55a6c4d
Elliott Loveridge <el627@bath.ac.uk>
2020-09-27 12:01:08 +0100

    calculate flops/params for resnet18

--- ogg-run.sh
+++ ogg-run.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# flops/params
+python /app/compressed-3d-cnn/calculate_FLOP.py
 
 ## benchmark - mobilenetv2
 
@@ -201,31 +203,31 @@
 
 ## knowledge distillation
 
-# ucf101-resnet-101 to resnet-18 knowledge distillation training (inc. testing)
-# NOTE: t_n_classes set as 101 as using fine-tuned teacher model
-python /app/compressed-3d-cnn/main.py --root_path /data \
---video_path ucf101_videos/jpg/ \
---annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
---result_path results \
---dataset ucf101 \
---n_classes 101 \
---batch_size 32 \
---model resnet \
---model_depth 18 \
---learning_rate 0.1 \
---n_val_samples 1 \
---n_epochs 20 \
---compress \
---compression_type kd \
---t_model resnet \
---t_model_depth 101 \
---t_path /data/results/benchmark/1209/ucf101_resnet_benchmark_20epochs_1209_best.pth \
---t_n_classes 101 \
---kd_distill_wt 0.7 \
---kd_student_wt 0.3 \
---kd_temp 5.0 \
---checkpoint 5 \
---test
+# # ucf101-resnet-101 to resnet-18 knowledge distillation training (inc. testing)
+# # NOTE: t_n_classes set as 101 as using fine-tuned teacher model
+# python /app/compressed-3d-cnn/main.py --root_path /data \
+# --video_path ucf101_videos/jpg/ \
+# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+# --result_path results \
+# --dataset ucf101 \
+# --n_classes 101 \
+# --batch_size 32 \
+# --model resnet \
+# --model_depth 18 \
+# --learning_rate 0.1 \
+# --n_val_samples 1 \
+# --n_epochs 20 \
+# --compress \
+# --compression_type kd \
+# --t_model resnet \
+# --t_model_depth 101 \
+# --t_path /data/results/benchmark/1209/ucf101_resnet_benchmark_20epochs_1209_best.pth \
+# --t_n_classes 101 \
+# --kd_distill_wt 0.7 \
+# --kd_student_wt 0.3 \
+# --kd_temp 5.0 \
+# --checkpoint 5 \
+# --test
 
 
 ## other
