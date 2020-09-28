5e3caa7993f787615eb6b873bf6b25aced49d134
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 17:26:02 +0100

    test only print of conv3d's

--- compressed-3d-cnn/new-prune.py
+++ compressed-3d-cnn/new-prune.py
@@ -4,8 +4,9 @@ path = '/app/compressed-3d-cnn/model_summary/mobilenetv2.csv'
 
 with open(path, newline='') as f:
     reader = pd.read_csv(f)
-    # data = list(reader)
+    reader = reader[reader['Type'] == 'Conv3d']
 
-# print(data)
-for index, row in reader.iterrows():
-    print(index, row)
+print(reader)
+
+# for index, row in reader.iterrows():
+#     print((reader['Name'], reader['Sparsity']))
