4d4e48a14173085337c093ce865e53d662280713
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 17:27:26 +0100

    typo

--- compressed-3d-cnn/new-prune.py
+++ compressed-3d-cnn/new-prune.py
@@ -6,5 +6,5 @@ with open(path, newline='') as f:
     df = pd.read_csv(f)
     df = df[df['Type']=='Conv3d']
 
-for index, row in reader.iterrows():
+for index, row in df.iterrows():
     print((reader['Name'], reader['Sparsity']))
