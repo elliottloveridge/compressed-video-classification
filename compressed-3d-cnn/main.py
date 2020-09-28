698b3e1c8f7d951140a9053e07b3112e8ff09917
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 18:02:00 +0100

    typo

--- compressed-3d-cnn/main.py
+++ compressed-3d-cnn/main.py
@@ -243,7 +243,7 @@ if __name__ == '__main__':
         quantizer.prepare_model(torch.rand(1, 3, 16, 112, 112))
 
     # test for parameter reduction
-    if opt.compression_type = 'ep':
+    if opt.compression_type == 'ep':
         par = sum(p.numel() - p.nonzero().size(0) for p in model.parameters() if p.requires_grad)
         print("post-compression zero parameter count:", par)
 
