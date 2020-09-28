35a40ee5e6f4e2b96b245a3bbc66cda2133cf39f
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 15:28:43 +0100

    distiller test for prune and fine-tune last layer

--- ogg-run.sh
+++ ogg-run.sh
@@ -2,14 +2,14 @@
 
 ## benchmark - mobilenetv2
 
-# # ucf101-mobilnetv2 (inc. testing) - 2 epochs, 0.1 learning rate, no checkpoint
+# # ucf101-mobilnetv2 (inc. testing) - 50 epochs, 0.1 learning rate, no checkpoint
 # python /app/compressed-3d-cnn/main.py --root_path /data \
 # --video_path ucf101_videos/jpg/ \
 # --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
 # --result_path results \
 # --dataset ucf101 \
 # --n_classes 101 \
-# --batch_size 64  \
+# --batch_size 32  \
 # --model mobilenetv2 \
 # --width_mult 1.0 \
 # --learning_rate 0.1 \
@@ -57,11 +57,11 @@
 
 ## evaluation
 
-# ucf101 evaluation (after testing)
-python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
---annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
---dataset ucf101 \
---result_path results
+# # ucf101 evaluation (after testing)
+# python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
+# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+# --dataset ucf101 \
+# --result_path results
 
 
 ## fine-tuning & transfer learning (pre-trained kinetics-600)
@@ -273,20 +273,19 @@ python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
 #   --pretrain_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
 
 
-# # distller pruning test only
-# python /app/compressed-3d-cnn/new-prune.py --root_path /data \
-#   --video_path ucf101_videos/jpg/ \
-#   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-#   --result_path results \
-#   --dataset ucf101 \
-#   --n_classes 101 \
-#   --batch_size 32  \
-#   --model mobilenetv2 \
-#   --width_mult 1.0 \
-#   --learning_rate 0.1 \
-#   --n_val_samples 1 \
-#   --n_epochs 1 \
-#   --resume_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
-#   --no_train \
-#   --no_val \
-#   --test
+# distller pruning test only
+python /app/compressed-3d-cnn/prune-last-layer.py --root_path /data \
+  --video_path ucf101_videos/jpg/ \
+  --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+  --result_path results \
+  --dataset ucf101 \
+  --n_classes 101 \
+  --batch_size 32  \
+  --model mobilenetv2 \
+  --width_mult 1.0 \
+  --learning_rate 0.01 \
+  --n_val_samples 1 \
+  --n_epochs 2 \
+  --resume_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
+  --ft_portion last_layer \
+  --test
