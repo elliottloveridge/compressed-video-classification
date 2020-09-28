2d20ba4994c7cf9d1c5f75d25660b2f54f4d4a42
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 17:26:58 +0100

    print set of name and sparsity

--- compressed-3d-cnn/new-prune.py
+++ compressed-3d-cnn/new-prune.py
@@ -3,10 +3,8 @@ import pandas as pd
 path = '/app/compressed-3d-cnn/model_summary/mobilenetv2.csv'
 
 with open(path, newline='') as f:
-    reader = pd.read_csv(f)
-    reader = reader[reader['Type'] == 'Conv3d']
+    df = pd.read_csv(f)
+    df = df[df['Type']=='Conv3d']
 
-print(reader)
-
-# for index, row in reader.iterrows():
-#     print((reader['Name'], reader['Sparsity']))
+for index, row in reader.iterrows():
+    print((reader['Name'], reader['Sparsity']))
