51c9a93695b9898c79d2e5f4659d59d9bb4ce1d8
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 17:30:32 +0100

    print sparsity loop

--- compressed-3d-cnn/new-prune.py
+++ compressed-3d-cnn/new-prune.py
@@ -6,5 +6,5 @@ with open(path, newline='') as f:
     df = pd.read_csv(f)
     df = df[df['Type']=='Conv3d']
 
-for index, row in df.iterrows():
-    print((df['Name'], df['Sparsity']))
+for i in df.index():
+    print((df['Name'][i], df['Sparsity'][i]))
