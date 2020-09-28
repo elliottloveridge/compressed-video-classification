ca6e2fb73ef8a0f6c1a17418a93887fcb156bcc2
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 17:04:28 +0100

    test pruning in script file

--- ogg-run.sh
+++ ogg-run.sh
@@ -57,11 +57,11 @@
 
 ## evaluation
 
-# ucf101 evaluation (after testing)
-python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
---annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
---dataset ucf101 \
---result_path results
+# # ucf101 evaluation (after testing)
+# python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
+# --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+# --dataset ucf101 \
+# --result_path results
 
 
 ## fine-tuning & transfer learning (pre-trained kinetics-600)
@@ -273,21 +273,21 @@ python /app/compressed-3d-cnn/utils/video_accuracy.py --root_path /data \
 #   --pretrain_path results/benchmark/1108/ucf101_mobilenetv2_50epochs_32batch-size_train-1108_best.pth
 
 
-# # distller pruning test only
-# python /app/compressed-3d-cnn/prune-last-layer.py --root_path /data \
-#   --video_path ucf101_videos/jpg/ \
-#   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
-#   --result_path results \
-#   --dataset ucf101 \
-#   --n_classes 101 \
-#   --batch_size 32  \
-#   --model mobilenetv2 \
-#   --width_mult 1.0 \
-#   --learning_rate 0.01 \
-#   --n_val_samples 1 \
-#   --n_epochs 2 \
-#   --compression_type ep-test \
-#   --pretrain_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
-#   --ft_portion last_layer \
-#   --compress \
-#   --test
+# distller pruning test only
+python /app/compressed-3d-cnn/prune-last-layer.py --root_path /data \
+  --video_path ucf101_videos/jpg/ \
+  --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
+  --result_path results \
+  --dataset ucf101 \
+  --n_classes 101 \
+  --batch_size 32  \
+  --model mobilenetv2 \
+  --width_mult 1.0 \
+  --learning_rate 0.01 \
+  --n_val_samples 1 \
+  --n_epochs 2 \
+  --compression_type ep-test \
+  --pretrain_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
+  --ft_portion last_layer \
+  --compress \
+  --test
