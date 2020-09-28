1601b4a7224ae9ab1b923780d3ce0ccdcfed80a5
Elliott Loveridge <el627@bath.ac.uk>
2020-09-28 17:19:15 +0100

    test pandas csv read

--- compressed-3d-cnn/new-prune.py
+++ compressed-3d-cnn/new-prune.py
@@ -1,9 +1,10 @@
-import csv
+import pandas as pd
 
 path = '/app/compressed-3d-cnn/model_summary/mobilenetv2.csv'
 
 with open(path, newline='') as f:
-    reader = csv.reader(f)
-    data = list(reader)
+    reader = pd.read_csv(f)
+    # data = list(reader)
 
-print(data)
+# print(data)
+print(reader)
