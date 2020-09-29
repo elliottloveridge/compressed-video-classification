403a4a8a5791584e4025b49651765ff1cde73af0
Elliott Loveridge <el627@bath.ac.uk>
2020-09-29 12:28:27 +0100

    test element wise compression on main.py

--- ogg-run.sh
+++ ogg-run.sh
@@ -181,7 +181,7 @@
 
 
 # element-wise pruning of mobilenetv2 w/ last_layer fine-tuning from pre-trained model
-python /app/compressed-3d-cnn/multi-compress.py --root_path /data \
+python /app/compressed-3d-cnn/main.py --root_path /data \
   --video_path ucf101_videos/jpg/ \
   --annotation_path /app/compressed-3d-cnn/annotation_UCF101/ucf101_01.json \
   --result_path results \
@@ -194,6 +194,7 @@ python /app/compressed-3d-cnn/multi-compress.py --root_path /data \
   --n_val_samples 1 \
   --n_epochs 1 \
   --compress \
+  --compression_type ep \
   --ft_portion last_layer \
   --pretrain_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
   --summary_path /app/compressed-3d-cnn/model_summary/mobilenetv2.csv \
