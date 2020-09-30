12d8db45da711108d9f031e447d7a09163d43e2f
Elliott Loveridge <el627@bath.ac.uk>
2020-09-30 13:14:12 +0100

    typo

--- ogg-run.sh
+++ ogg-run.sh
@@ -198,8 +198,6 @@ python /app/compressed-3d-cnn/main.py --root_path /data \
   --ft_portion last_layer \
   --pretrain_path results/benchmark/1009/ucf101_mobilenetv2_benchmark_20epochs_1009_best.pth \
   --summary_path /app/compressed-3d-cnn/model_summary/mobilenetv2.csv \
-  --no_train \
-  --no_val \
   --test
 
 
